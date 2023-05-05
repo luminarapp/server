610e02f66485e8c899e9edd124364df8094d199b
chroxify <chvtodorov@gmail.com>
2023-05-05 02:08:37 +0200

    Implement user routes and authentication

---
+++ controllers/user.go
@@ -0,0 +1,199 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lithammer/shortuuid/v4"
+	"github.com/luminarapp/server/auth"
+	"github.com/luminarapp/server/config"
+	"github.com/luminarapp/server/models"
+	"github.com/luminarapp/server/utils"
+)
+
+// GET /users/me
+func CurrentUser(c *gin.Context) {
+	userId, err := auth.ExtractTokenID(c)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var user models.User
+
+	if err := models.DB.Where("id = ?", userId).First(&user).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": user})
+}
+
+// GET /users/:id
+func GetUser(c *gin.Context) {
+	var user models.User
+
+	// Preload captures and collections
+	if err := models.DB.Preload("Captures").Preload("Collections").Where("id = ?", c.Param("id")).First(&user).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "user id not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": user})
+}
+
+// POST /users
+func CreateUser(c *gin.Context) {
+	var payload models.CreateUserRequest
+
+	if err := c.ShouldBindJSON(&payload); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user := models.User{
+		ID: shortuuid.New(),
+		Email: payload.Email,
+	}
+
+	if err := models.DB.Create(&user).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+
+	
+	c.JSON(http.StatusOK, gin.H{"data": user})
+}
+
+// POST /users/auth
+func UserAuthChallenge(c *gin.Context) {
+	var payload models.UserAuthChallengeRequest
+
+	if err := c.ShouldBindJSON(&payload); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Check if token provided in payload
+	if payload.Token == "" || payload.Token == "null" {
+		// Check if user exists with email
+		var user models.User
+
+		// Generate magic token
+		magicToken := utils.GenerateMagicToken()
+
+		if err := models.DB.Where("email = ?", payload.Email).First(&user).Error; err != nil {
+			// Create user
+			user = models.User{
+				ID: shortuuid.New(),
+				Username: strings.ToLower(strings.Split(payload.Email, "@")[0]),
+				Email: payload.Email,
+				AuthToken: models.AuthToken{
+					Token: magicToken,
+					ExpiresAt: time.Now().Add(time.Minute * time.Duration(config.Config().ChallengeTokenLifetime)).Unix(),
+				},
+			}
+
+			if err := models.DB.Create(&user).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{"data": models.UserAuthChallengeStatus{
+					Success: false,
+					AuthStep: "tokenRequest",
+					ErrorMsg: err.Error(),
+				}})
+
+				return
+			}
+
+			c.JSON(http.StatusOK, gin.H{"data": models.UserAuthChallengeStatus{
+				Success: true,
+				AuthStep: "tokenRequest",
+			}})
+
+			return
+		} else {
+			// Update user token
+			user.AuthToken = models.AuthToken{
+				Token: magicToken,
+				ExpiresAt: time.Now().Add(time.Minute * time.Duration(config.Config().ChallengeTokenLifetime)).Unix(),
+			}
+
+			if err := models.DB.Save(&user).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{"data": models.UserAuthChallengeStatus{
+					Success: false,
+					AuthStep: "tokenRequest",
+					ErrorMsg: err.Error(),
+				}})
+				return
+			}
+
+			fmt.Println("Auth token (Request): ", user.AuthToken.Token)
+
+			c.JSON(http.StatusOK, gin.H{"data": models.UserAuthChallengeStatus{
+				Success: true,
+				AuthStep: "tokenRequest",
+			}})
+
+			return
+		}
+	}
+
+	// Check if token provided in payload is valid
+	var user models.User
+
+	// Get user by email
+	if err := models.DB.Where("email = ?", payload.Email).First(&user).Error; err != nil {
+		c.JSON(http.StatusOK, gin.H{"data": models.UserAuthChallengeStatus{
+			Success: false,
+			AuthStep: "tokenSubmittion",
+			ErrorMsg: "user not found",
+		}})
+	
+		return
+	}
+
+	fmt.Println("Auth token (Submittion): ", payload.Token)
+	fmt.Println("Current Auth token: ", user.AuthToken.Token)
+
+	// Check if token matches
+	if payload.Token != user.AuthToken.Token {
+		c.JSON(http.StatusOK, gin.H{"data": models.UserAuthChallengeStatus{
+			Success: false,
+			AuthStep: "tokenSubmittion",
+			ErrorMsg: "invalid token",
+		}})
+		return
+	}
+
+	// Check if token has expired
+	if user.AuthToken.ExpiresAt < time.Now().Unix() {
+		c.JSON(http.StatusOK, gin.H{"data": models.UserAuthChallengeStatus{
+			Success: false,
+			AuthStep: "tokenSubmittion",
+			ErrorMsg: "token expired",
+		}})
+		return
+	}
+
+	// Delete token
+	user.AuthToken = models.AuthToken{}
+
+	if err := models.DB.Save(&user).Error; err != nil {
+		c.JSON(http.StatusOK, gin.H{"data": models.UserAuthChallengeStatus{
+			Success: false,
+			AuthStep: "tokenSubmittion",
+			ErrorMsg: err.Error(),
+		}})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": models.UserAuthChallengeStatus{
+		Success: true,
+		AuthStep: "tokenSubmittion",
+		Session: auth.GenerateToken(user.ID),
+	}})
+}
---
+++ models/user.go
@@ -0,0 +1,33 @@
+package models
+
+type User struct {
+	ID     string `gorm:"primary_key;unique" json:"id"` // Unique User ID
+	Username   string `json:"username"` // Username of the user
+	Email  string `json:"email"` // Email of the user
+	Collections []Collection `gorm:"foreignKey:UserID;default:'{}'" json:"-"` // Array of collections 
+	Captures []Capture `gorm:"foreignKey:UserID;default:'{}'" json:"-"` // Array of captures
+	AuthToken AuthToken `gorm:"embedded" json:"-"` // Auth token of the user
+	UpdatedAt   int64 `gorm:"autoUpdateTime" json:"updated_at"` // Unix timestamp of when the user was last updated
+	CreatedAt   int64 `gorm:"autoCreateTime" json:"created_at"` // Unix timestamp of when the user was created
+}
+
+type CreateUserRequest struct {
+	Email string `json:"email" binding:"required"`
+}
+
+type AuthToken struct {
+	Token string `json:"token"`
+	ExpiresAt int64 `json:"expires_at"`
+}
+
+type UserAuthChallengeStatus struct {
+	Success bool `json:"success"`
+	AuthStep string `json:"authStep"`
+	ErrorMsg string `json:"errorMsg,omitempty"` 
+	Session string `json:"session,omitempty"`
+}
+
+type UserAuthChallengeRequest struct {
+	Email string `json:"email" binding:"required"`
+	Token string `json:"token"` // If token is not provided, a new token will be generated and sent to the user's email
+}
